e9dd1db7420a9df82683cc3e4cfdd278b076a0f6
Jacob Merson <mersoj2@rpi.edu>
2025-09-08 02:59:10 -0400

    update CABANA scripts to work with NATIVE build

--- configure-and-build-cabana.sh
+++ configure-and-build-cabana.sh
@@ -7,12 +7,18 @@ export NVCC_WRAPPER_DEFAULT_COMPILER=`which mpicxx`
 
 git clone https://github.com/ECP-copa/Cabana.git
 
+ENABLE_CUDA=ON
+if [ "${DEVICE_ARCH}" = "NATIVE" ]; then
+  echo "BUILDING WITH NATIVE CPU ARCH"
+  ENABLE_CUDA=OFF
+fi
+
 cmake -S $SOURCE_DIR/Cabana -B $BUILD_DIR/${DEVICE_ARCH}/Cabana \
   -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/${DEVICE_ARCH}/Cabana/install \
   -DCMAKE_CXX_COMPILER=`which mpicxx` \
   -DCMAKE_C_COMPILER=`which mpicc` \
   -DKokkos_DIR=$BUILD_DIR/${DEVICE_ARCH}/kokkos/install/lib64/cmake/Kokkos/ \
-  -DCabana_REQUIRE_CUDA=ON \
+  -DCabana_REQUIRE_CUDA=${ENABLE_CUDA} \
   -DCabana_ENABLE_TESTING=OFF \
   -DCabana_ENABLE_EXAMPLES=OFF
 
