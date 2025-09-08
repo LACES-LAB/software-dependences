33a2fd42b237429dfc474d198ae03af714cb52c3
Jacob Merson <mersoj2@rpi.edu>
2025-09-08 02:09:58 -0400

    export NVCC_WRAPPER

--- loads-rhel9.sh
+++ loads-rhel9.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 module purge
 module use /opt/scorec/spack/rhel9/v0201_4/lmod/linux-rhel9-x86_64/Core/
 module load gcc/12.3.0-iil3lno mpich/4.1.1-xpoyz4t cuda/12.1.1-zxa4msk
@@ -12,3 +14,4 @@ module load simmetrix-simmodsuite/2025.0-250108dev-llxq6sk
 module load netlib-lapack/3.11.0-b22mgwg #netlib-lapack includes blas
 module load netlib-scalapack/2.2.0-fzd4jvl
 
+export NVCC_WRAPPER_DEFAULT_COMPILER=`which mpicxx`
